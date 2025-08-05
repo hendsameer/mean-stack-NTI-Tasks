2bb9c9c70f4cf8bdc26216676d14607ef57b40bd
hendsameer <sameerhend719@gmail.com>
2025-08-05 10:46:29 +0300

    Create script.js

---
+++ Task1-JS/script.js
@@ -0,0 +1,28 @@
+let String = "hend Sameer";
+let Number = 2222;
+let Boolean = false;
+let Null = null;
+let Undefined;
+
+console.log("String:", String);
+console.log("Number:", Number);
+console.log("Boolean:", Boolean);
+console.log("Null:", Null);
+console.log("Undefined:", Undefined);
+
+
+console.log("Type of String:", typeof String);
+console.log("Type of Number:", typeof Number);
+console.log("Type of Boolean:", typeof Boolean);
+console.log("Type of Null:", typeof Null);
+console.log("Type of Undefined:", typeof Undefined);
+
+
+let firstName = "hend";
+let lastName = "sameer";
+
+let fullNameOld = firstName + " " + lastName;
+console.log("Concatenated Name :", fullNameOld);
+
+let fullNameNew = `Concatenated Name : ${firstName} ${lastName}`;
+console.log(fullNameNew);
